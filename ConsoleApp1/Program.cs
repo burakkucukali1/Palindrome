d3109c17ba8a9d990014a80629254239a0eeb427
Burak <51200138+burakkucukali1@users.noreply.github.com>
2019-05-30 19:16:15 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+		public static bool FirstMethod(string str)
+		{
+			int i = 0;
+			int j = str.Length - 1;
+
+			while (i < j)
+			{
+				if (str[i] != str[j])
+					return false;
+
+				i++;
+				j--;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Palindrome();
+			Bekle();
+
+		}
+		static void Bekle()
+		{
+			Console.ReadKey();
+		}
+		static void EkranaYaz(string metin)
+		{
+			Console.WriteLine(metin);
+		}
+		static void EkranaYaz(int sayi)
+		{
+			EkranaYaz(sayi.ToString());
+		}
+		static string EkrandanOku()
+		{
+			return Console.ReadLine();
+		}
+
+		static void Palindrome()
+		{
+			EkranaYaz("Bir tamsayı giriniz: ");
+			string metin = EkrandanOku();
+			int metin1;
+
+			bool donustuMu = int.TryParse(metin, out metin1);
+
+			if (!donustuMu)
+			{
+				EkranaYaz("Lütfen doğru formatta bir sayı yazınız.");
+			}
+			else
+			{
+				string revs = "";
+				for (int i = metin.Length - 1; i >= 0; i--)
+				{
+					revs += metin[i].ToString();
+				}
+				if (revs == metin)
+				{
+					EkranaYaz("Girdiğiniz sayı bir palindromdur.");
+				}
+				else
+				{
+					EkranaYaz("Girdiğiniz sayı bir palindrom değildir.");
+				}
+			}
+			if (metin=="exit")
+			{
+				Environment.Exit(0);
+			}
+		}
+	}
+}
+	
